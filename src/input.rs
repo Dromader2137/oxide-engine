163311ff4d792a5d3ebb11b353f296937d831274
Dromader2137 <wojciechowski.ignacy@gmail.com>
2024-02-25 19:26:55 +0100

    Input update

--- src/input.rs
+++ src/input.rs
@@ -2,11 +2,16 @@ use std::collections::HashSet;
 
 use winit::keyboard::Key;
 
+use crate::{types::vectors::Vec2f, ecs::{System, World}, rendering::Renderer};
+
 #[derive(Clone, Debug)]
 pub struct InputManager {
     pub pressed: HashSet<Key>,
     pub down: HashSet<Key>,
     pub released: HashSet<Key>,
+
+    pub mouse_pos: Vec2f,
+    prev_mouse_pos: Option<Vec2f>,
 }
 
 impl InputManager {
@@ -22,16 +27,38 @@ impl InputManager {
         self.released.insert(key_code);
     }
 
+    pub fn get_mouse_delta(&self) -> Vec2f {
+        if self.prev_mouse_pos.is_none() { 
+            Vec2f::new([0.0, 0.0])
+        } else { 
+            Vec2f::new([self.mouse_pos.x - self.prev_mouse_pos.unwrap().x, self.mouse_pos.y - self.prev_mouse_pos.unwrap().y])
+        }
+    }
+
     pub fn clear_temp(&mut self) {
         self.pressed.clear();
         self.released.clear();
+        self.prev_mouse_pos = Some(self.mouse_pos.clone());
     }
 
     pub fn new() -> InputManager {
         InputManager { 
             pressed: HashSet::new(), 
             down: HashSet::new(), 
-            released: HashSet::new() 
+            released: HashSet::new(),
+            mouse_pos: Vec2f::new([0.0, 0.0]),
+            prev_mouse_pos: None
         }
+    }
+}
+
+struct InputManagerUpdater {}
+
+impl System for InputManagerUpdater {
+    fn on_start(&self, _world: &World, _renderer: &mut Renderer) {}
+    fn on_update(&self, world: &World, _renderer: &mut Renderer) {
+        let mut input_manager_list = world.borrow_component_vec_mut::<InputManager>().unwrap();
+        let input_manager = input_manager_list.iter_mut().next().unwrap().as_mut().unwrap();
+        input_manager.clear_temp();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,12 @@ use input::InputManager;
 use rendering::{EventLoop, Renderer, ShaderManager, Window, CameraUpdater, MeshUpdater};
 use types::transform::TransformUpdater;
 
-use winit::{event::{Event, WindowEvent::{self, KeyboardInput}, KeyEvent, ElementState}, event_loop::ControlFlow};
+use winit::dpi::LogicalPosition;
+use winit::event::{Event, WindowEvent, KeyEvent, ElementState};
+use winit::event_loop::ControlFlow;
+use winit::event::WindowEvent::KeyboardInput;
+use winit::event::DeviceEvent::MouseMotion;
+use winit::window::CursorGrabMode;
 
 pub fn run(mut world: World) {
     let event_loop = EventLoop::new();
@@ -27,6 +32,8 @@ pub fn run(mut world: World) {
 
     renderer.update_command_buffers(&mut world, &shader_manager);
     event_loop.event_loop.set_control_flow(ControlFlow::Poll);
+    window.window_handle.set_cursor_grab(CursorGrabMode::Locked).unwrap();
+    window.window_handle.set_cursor_visible(false);
     event_loop
         .event_loop
         .run(move |event, elwt| match event {
@@ -74,14 +81,29 @@ pub fn run(mut world: World) {
                 let mut input_manager = input_manager_list.iter_mut();
                 input_manager.next().unwrap().as_mut().unwrap().process_key_release(key_code);
             }
+            Event::DeviceEvent { 
+                event: MouseMotion {
+                    delta: (x, y)
+                },
+                ..
+            } => {
+                let mut input_manager_list = world.borrow_component_vec_mut::<InputManager>().unwrap();
+                let input_manager = input_manager_list.iter_mut().next().unwrap().as_mut().unwrap();
+                input_manager.mouse_pos.x += x as f32;
+                input_manager.mouse_pos.y += y as f32;
+            }
             Event::AboutToWait => {
                 renderer.handle_possible_resize(&window,  &mut world, &mut shader_manager);
                 renderer.render();
                 renderer.wait_for_idle();
+                
                 world.update(&mut renderer);
+                
                 let mut input_manager_list = world.borrow_component_vec_mut::<InputManager>().unwrap();
                 let input_manager = input_manager_list.iter_mut().next().unwrap().as_mut().unwrap();
                 input_manager.clear_temp();
+                let size = window.window_handle.inner_size();
+                window.window_handle.set_cursor_position(LogicalPosition::new(size.width / 2, size.height / 2)).unwrap();
             }
             _ => (),
         }
